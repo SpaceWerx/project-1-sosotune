a67995eca9a05d0259f2b8e4823d7cb56f0f8e3f
sosotune <tslxg2019@gmail.com>
2022-05-05 17:00:06 -0400

    added user service skeleton, added reimbursement service code

---
+++ Project1/src/main/java/com/revature/Service/User_Service.java
@@ -0,0 +1,24 @@
+package com.revature.Service;
+import java.util.List;
+
+import com.revature.Model.*;
+
+public class User_Service {
+	public void getUser(String userName){
+	
+	}
+	public void getUser(int id) {
+		
+	}
+	public void getAllUsers(List<User> users) {
+		
+	}
+	public void idExists(int id) {
+		
+	}
+	
+	public void getUser(Role role) {
+		
+	}
+
+}
