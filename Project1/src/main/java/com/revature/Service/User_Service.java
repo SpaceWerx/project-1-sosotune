ff49ed614b9c2c976044d6bbdfe09de7089430f1
sosotune <tslxg2019@gmail.com>
2022-05-10 14:47:07 -0400

    add method bodies for userservice

--- Project1/src/main/java/com/revature/Service/User_Service.java
+++ Project1/src/main/java/com/revature/Service/User_Service.java
@@ -1,26 +1,52 @@
 package com.revature.Service;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.Model.*;
 
 public class User_Service {
-	public User getUserByUsername(String username){
-		return null;
+	private final List<User> users = new ArrayList<>();
 	
+	
+	public User getUserByUsername(String username){
+		for (User user : users) {
+			if (user.getUsername() == username) {
+				return user;
+			}
+		}
+		return null;
 	}
 	public User getUserById(int id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
 		return null;
 		
 	}
+	
 	public List<User> getAllUsers(List<User> users) {
 		return users;
 		
 	}
 	public void idExists(int id) {
+		for (User user: users) {
+			if(user.getId()==id) {
+				System.out.println("Id exists.");
+				break;
+			}
+		}
+		System.out.println("Id does not exist");
 		
 	}
 	
-	public List<User> getByRole(Role role) {
+	public User getByRole(Role role) {
+		for(User user: users) {
+			if(user.getRole() == role) {
+				return user;
+			}
+		}
 		return null;
 		
 	}
