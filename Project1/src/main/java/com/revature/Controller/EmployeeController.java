831353dff84bc24d7b68f71c3d462f630e145fd9
sosotune <tslxg2019@gmail.com>
2022-05-18 08:47:30 -0400

    addedjavalin

---
+++ Project1/src/main/java/com/revature/Controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.revature.Controller;
+
+import io.javalin.http.Handler;
+
+public class EmployeeController {
+
+	public Handler getEmployeesHandler;
+	public Handler insertEmployeesHandler;
+
+}
--- Project1/src/main/java/com/revature/Launcher.java
+++ Project1/src/main/java/com/revature/Launcher.java
@@ -1,21 +1,32 @@
 package com.revature;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Scanner;
 
+import com.revature.Controller.EmployeeController;
 import com.revature.DAO.ReimbursementDAO;
 import com.revature.DAO.UserDAO;
 import com.revature.Model.Reimbursement;
 import com.revature.Model.Role;
 import com.revature.Model.Status;
 import com.revature.Service.*;
+import com.revature.Utility.ConnectionFactoryUtility;
+
+import io.javalin.Javalin;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		ReimbursementService reim = new ReimbursementService();
+		EmployeeController ec = new EmployeeController();
+		try(Connection conn = ConnectionFactoryUtility.getConnection()){
+			System.out.println("Connection Successful :)");
+		} catch (SQLException e) {
+			System.out.println("Connection failed");
+			e.printStackTrace();
+		}
+		//ReimbursementService reim = new ReimbursementService();
 		//ReimbursementDAO rdao = new ReimbursementDAO();
 		
 		//for(Reimbursement r : reim.getReimbursementByAuthor(1)) {
@@ -28,7 +39,15 @@ public class Launcher {
 		//System.out.println(userService.getByRole(Role.MANAGER));
 		CLI_Menu_Service options = new CLI_Menu_Service();
 		options.displayLoginMenu();
-		
+//		Javalin app = Javalin.create(
+//			config -> {
+//				config.enableCorsForAllOrigins();
+//			}
+//		).start(3000);
+//		
+//		app.get("/employee", ec.getEmployeesHandler );
+//		
+//		app.post("/employee", ec.insertEmployeesHandler);
 	}
 
 }
