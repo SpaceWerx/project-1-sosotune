79dba7aae34c6cb9053e66af153cb1fbd076e9fe
sosotune <tslxg2019@gmail.com>
2022-05-20 14:28:55 -0400

    addedauthcont

--- Project1/src/main/java/com/revature/Controller/AuthController.java
+++ Project1/src/main/java/com/revature/Controller/AuthController.java
@@ -1,10 +1,45 @@
 package com.revature.Controller;
 
+import com.google.gson.Gson;
+import com.revature.DAO.UserDAO;
+import com.revature.Model.User;
+import com.revature.Service.AuthService;
+
 import io.javalin.http.Handler;
 
 public class AuthController {
+	
+	AuthService as = new AuthService();
+	UserDAO userDAO = new UserDAO();
+	User user = new User();
 
-	public Handler getLoginHandler;
-	public Handler getRegisterHandler;
+	public Handler getLoginHandler = (ctx) -> {
+		String body = ctx.body();
+		
+		Gson gson = new Gson();
+		
+		user = gson.fromJson(body, User.class);
+		
+		
+		if(as.login(user.getUsername(), user.getPassword()) != null) {
+			ctx.status(201);
+			ctx.result("Login Successful");
+			
+		}
+		else {
+			ctx.status(404);
+			ctx.result("Password or username incorrect");
+		}
+	};
+	public Handler getRegisterHandler = (ctx) -> {
+		String body = ctx.body();
+		Gson gson = new Gson();
+		
+		user = gson.fromJson(body, User.class);
+		userDAO.createUser(user);
+		
+		ctx.result("User successfully added");
+		ctx.status(201);
+	};
 
 }
